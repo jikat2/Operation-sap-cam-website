dcb17aaacedd43f45e1eb228b1c0eb2de91e42f5
jikat2 <61894479+jikat2@users.noreply.github.com>
2021-01-30 20:41:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ window.onload = function(){
 		var distance = document.getElementById("myKm").value;
 		var data = {"latitude":latitude,"longitude":longitude,"distance":distance};
 		const sendPost = async () => {
-			const url = 'https://localhost:3000/test';
+			const url = 'http://localhost:5000/test';
 			const body = JSON.stringify(data);
 			const headers = {'Content-Type': 'application/json'}; 
 			const method = 'POST';
 			const response = await fetch(url, { method, body, headers })
 			const data2 = await response.text();
+			return data2
 		}
 
-		sendPost();
+		currentDiv = document.getElementById("div1");
+		otherDiv = document.getElementById("div2");
+		sendPost().then(data => {currentDiv.innerHTML = "<img src='plot2.png'>"; otherDiv.innerHTML = data; });
 		
 	}
 };
